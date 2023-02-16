aaf001f24f8cb6180d7cb1259c8518277ab76da7
Vinod Jatav <jatavvinod1997@gmail.com>
2023-02-16 23:58:28 +0530

    adding some code

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Login from "./components/Login";
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      <Home/>
+      <Login />
+      {/* <Home/> */}
     </div>
   );
 }
--- src/components/SwipeableTextMobileStepper.tsx
+++ src/components/SwipeableTextMobileStepper.tsx
@@ -3,7 +3,7 @@ import { useTheme } from "@mui/material/styles";
 import MobileStepper from "@mui/material/MobileStepper";
 import SwipeableViews from "react-swipeable-views";
 import { autoPlay } from "react-swipeable-views-utils";
-import { Box } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import { FirstCardIcons, SecondCardIcons, ThirdCardIcons } from "./Icons";
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
@@ -11,15 +11,21 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 const cards = [
   {
     id: "FirstCard",
-    cardPath: <FirstCardIcons sx={{ width: "299px", height: "419px" }} />,
+    title: "Enjoy Your Time",
+    message: "when you are confused about \n managing your task come to us",
+    cardPath: <FirstCardIcons sx={{ width: "299px", height: "277px" }} />,
   },
   {
     id: "SecondCard",
-    cardPath: <SecondCardIcons sx={{ width: "299px", height: "419px" }} />,
+    title: "Enjoy Your Time",
+    message: "when you are confused about managing your task come to us",
+    cardPath: <SecondCardIcons sx={{ width: "299px", height: "277px" }} />,
   },
   {
     id: "ThirdCard",
-    cardPath: <ThirdCardIcons sx={{ width: "299px", height: "419px" }} />,
+    title: "Enjoy Your Time",
+    message: "when you are confused about managing your task come to us",
+    cardPath: <ThirdCardIcons sx={{ width: "299px", height: "277px" }} />,
   },
 ];
 
@@ -56,7 +62,7 @@ const SwipeableTextMobileStepper = () => {
       }}
     >
       <AutoPlaySwipeableViews
-        style={{overflow: "hidden"}}
+        style={{ overflow: "hidden" }}
         axis={theme.direction === "rtl" ? "x-reverse" : "x"}
         index={activeStep}
         onChangeIndex={handleStepChange}
@@ -65,7 +71,59 @@ const SwipeableTextMobileStepper = () => {
         {cards.map((step, index) => (
           <div key={step.id}>
             {Math.abs(activeStep - index) <= 2 ? (
-              <Box>{step.cardPath}</Box>
+              <Box>
+                {step.cardPath}
+                <Stack
+                  sx={{
+                    flexDirection: "column",
+                    alignItems: "center",
+                    padding: "0",
+                    gap: "30px",
+                    // width: "266px",
+                    // height: "102px",
+                    flex: "none",
+                    order: "1",
+                    flexFlow: "0",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      mt: "40px",
+                      fontFamily: "Poppins",
+                      fontStyle: "normal",
+                      fontWeight: "700",
+                      fontSize: "32px",
+                      letterSpacing: "0.01em",
+                      color: "#363942",
+                      flex: "none",
+                      order: "0",
+                      flexGrow: "0",
+                    }}
+                  >
+                    {step.title}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      // display: "inline-block",
+                      // width: "260px",
+                      fontFamily: "Poppins",
+                      fontStyle: "normal",
+                      fontWeight: "400",
+                      fontSize: "14px",
+                      letterSpacing: "0.01em",
+                      textAlign: "center",
+                      textTransform: "capitalize",
+                      color: "#363942",
+                      opacity: "0.6",
+                      flex: "none",
+                      order: "1",
+                      flexGrow: "0",
+                    }}
+                  >
+                    {step.message}
+                  </Typography>
+                </Stack>
+              </Box>
             ) : null}
           </div>
         ))}
